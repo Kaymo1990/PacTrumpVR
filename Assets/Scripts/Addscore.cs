295e9eb6bf6aa92f549ef0503938b9bf7c6c98fc
Khurram Mohammed <khurrammohammed1990@gmail.com>
2019-09-06 11:33:32 +0100

    added teleporters and scoring

---
+++ Assets/Scripts/Addscore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addscore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+
+        SumScore.Add(10);
+
+        SumScore.SaveHighScore();
+    }
+}
