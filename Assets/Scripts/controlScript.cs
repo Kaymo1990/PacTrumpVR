3ea76ba0f242809cc58d5184ebc4dfe9f730dbca
Kareem <kareem335@yahoo.co.uk>
2019-09-06 14:58:38 +0100

    intro scene and in game scoring ui integrated

---
+++ Assets/Scripts/controlScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controlScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("PacTrump2.0");
+    }
+}
