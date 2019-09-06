86293f6818ebc7930d1197f1a176789c20ba5b47
Student <student@Makerss-MacBook-Pro.local>
2019-09-06 18:16:35 +0100

    player with lives added

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit col)     {         if (col.gameObject.name == "TrumpAI" || col.gameObject.name == "TrumpAI(Clone)")
+        {             SceneManager.LoadScene("intro"); ;
+
+        }
+
+    }
+}
